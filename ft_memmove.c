2167df5f340dda3cd1ee613e166dfcdd2fee9581
Will Taylor <will@webwhizz.com>
2018-05-15 21:44:41 +0000

    Updated libft memmove prototype

--- ft_memmove.c
+++ ft_memmove.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	char	*dst2;
 	char	*src2;

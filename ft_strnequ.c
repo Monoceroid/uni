9006edbadd79042dff68616f74896733b9e0a9c2
William TAYLOR <wtaylor@e1r5p11.42.fr>
2018-06-06 19:01:39 +0200

    Going to submit. May come back and add bonus functions later.

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: wtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/19 15:21:10 by wtaylor           #+#    #+#             */
-/*   Updated: 2018/06/05 19:12:01 by wtaylor          ###   ########.fr       */
+/*   Updated: 2018/06/06 18:56:43 by wtaylor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 
 	if (n == 0)
 		return (1);
-	if (s1 && s2 && n)
+	if (s1 && s2)
 	{
 		i = 0;
 		while (i < n)

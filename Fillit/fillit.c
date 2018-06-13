f83683ca617a5b6c5996392b77234b092174d567
William TAYLOR <wtaylor@e1r6p7.42.fr>
2018-06-13 17:19:34 +0200

    Fillit now testing one line of input file for errors and writing to array.

--- Fillit/fillit.c
+++ Fillit/fillit.c
@@ -6,7 +6,7 @@
 /*   By: wtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 18:59:11 by wtaylor           #+#    #+#             */
-/*   Updated: 2018/06/13 14:57:10 by wtaylor          ###   ########.fr       */
+/*   Updated: 2018/06/13 17:17:46 by wtaylor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,43 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
+
+int	tetrimino(int fd)
+{
+	char	buffer;
+	char	tet[5][5];
+	int		count;
+	int		i;
+
+	i = 0;
+	buffer = 'a';
+	count = 0;
+	while (buffer != '\n')
+	{
+		read(fd, &buffer, 1);
+		count++;
+		if (count != 5)
+		{
+			if (buffer != '.' && buffer != '#')
+				return (0);
+		}
+		else
+		{
+			if (buffer != '\n')
+				return (0);
+		}
+		tet[0][i] = buffer;
+		printf("%c", tet[0][i]);
+		i++;
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
 	int		i;
 	int		fd;
-	char	buffer;
 
 	if (argc != 2)
 	{
@@ -28,10 +59,11 @@ int	main(int argc, char **argv)
 	}
 	i = 0;
 	fd = open(argv[1], O_RDONLY);
-	while (buffer != '\n')
+	if (tetrimino(fd) == 0)
 	{
-		read(fd, &buffer, 1); 
-		write(1, &buffer, 1);
+		ft_putstr("error");
+		ft_putchar('\n');
+		return (0);
 	}
 	close(fd);
 	return (0);

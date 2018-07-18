55cb4a408f9e2dd22ba454b678b9332f16bd8af3
Will Taylor <will@webwhizz.com>
2018-07-18 07:24:40 +0000

    Ensuring all work done on cs50.io is pushed.

--- Fillit/Libft/ft_arrstr.c
+++ Fillit/Libft/ft_arrstr.c
@@ -5,5 +5,9 @@ int *ft_arrstr(char *str, char c, int i, int j)
     while (*str != '\0')
     {
         if (*str >= '0' && *str <= '9' && *(str + 1) == c)
+            arr[i][j] = *str++ - '0';
+        else if (*str >= '0' && *str <= '9')
+            arr[i][j] = (*str - '0') * 10 + *(str++ + 1);
+
     }
 }

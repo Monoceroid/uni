549b067fa173226aac32933d355143bf9827dcbe
William TAYLOR <wtaylor@e1r5p7.42.fr>
2018-07-23 12:51:28 +0200

    Wrote generalised function ft_1ardif which compares array p to array q for any matching elements, as opposed to previous ft_1arrdif which just looked for repeated elements within itself. Believe this is necessary for the planned ft_2ardif looking at 2D arrays.

---
+++ Fillit/Libft/ft_1ardif.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_1ardif.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/23 12:39:43 by wtaylor           #+#    #+#             */
+/*   Updated: 2018/07/23 12:49:41 by wtaylor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	ft_1ardif(int *p, int *q, int n, int count)
+{
+	int	res;
+	int	x;
+
+	res = 1;
+	if (p == q)
+		x = 0;
+	else
+		x = -1;
+	if (count == 1)
+		return (res);
+	while (++x < n)
+	{
+		if (*p == *(q + x))
+			res = 0;
+	}
+	if (res == 0)
+		return (res);
+	else
+		res = ft_1ardif(p + 1, q, n, count - 1);
+	return (res);
+}
+
+int	main(void)
+{
+
+	int n;
+	int *p;
+	int	*q;
+	int count;
+	int	i;
+
+	n = 12;
+	p  = (int *)malloc(sizeof(int ) * n);
+	count = 1;
+	i = 0;
+	while (i < 12)
+		p[i++] = count++;
+	i = 0;
+	while (i < 12)
+		printf("%d ", p[i++]);
+	q  = (int *)malloc(sizeof(int ) * n);
+	count = 13;
+	i = 0;
+	while (i < 12)
+		q[i++] = count++;
+	q[10] = 8;
+	i = 0;
+	printf("\n");
+	while (i < 12)
+		printf("%d ", q[i++]);
+	printf("\n%d", ft_1ardif(p, q, 12, 12));
+	return (0);
+}
